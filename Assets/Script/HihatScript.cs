80d8ef0bcb80165cbaa8c1c75ac4ff17d53d9035
MinJaePark <qkr2817@gmail.com>
2024-10-10 16:38:00 +0900

    :sparkles: Feat : 하이햇 페달누름에 따른 하이햇 소리 구현

--- Assets/Script/HihatScript.cs
+++ Assets/Script/HihatScript.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HihatScript : MonoBehaviour
 {
     AudioSource audioSource;
+    public AudioClip closeHiHatClip;
+    public AudioClip openHiHatClip;
+    public PedalScript pS;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        //closeHiHatClip = Resources.Load("drumSound/close hihat") as AudioClip;
+        //openHiHatClip = Resources.Load("drumSound/open hihat") as AudioClip;
+
+        //pS = GetComponent<PedalScript>();
     }
 
     // Update is called once per frame
@@ -17,16 +28,43 @@ public class HihatScript : MonoBehaviour
 
     }
 
-    void OnTriggerEnter(Collider other)
+
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("하이헷 충돌 시작");
-        audioSource.PlayOneShot(audioSource.clip);
+        if (other.CompareTag("HiHat") && pS.GetIsPedalPressed())
+        {
+            Debug.Log("닫은 하이햇 충돌소리");
+            audioSource.PlayOneShot(closeHiHatClip);
+        }
+        else
+        {
+            Debug.Log("열린 하이햇 충돌소리");
+            audioSource.PlayOneShot(openHiHatClip);
+        }
     }
 
-    void OnTriggerExit(Collider other)
-    {
-        Debug.Log("충돌 끝");
-    }
 
+
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("HiHat"))
+        {
+            if (pS.GetPedalPressed())
+            {
+                Debug.Log("클로즈 하이헷 충돌 - 클로즈 하이헷 소리 재생");
+                audioSource.PlayOneShot(closeHiHatClip);
+            }
+            else
+            {
+                Debug.Log("오픈 하이헷 충돌 - 오픈 하이헷 소리 재생");
+                audioSource.PlayOneShot(openHiHatClip);
+            }
+        }
+    }
+*/
 
 }
+
+
+
